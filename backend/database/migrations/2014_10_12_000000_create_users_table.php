4fc8eb33daf1018c32bda1479941a75fd9c1a5a6
mkawsar <kawsar.diu888@gmail.com>
2023-11-10 17:39:59 +0600

    added file uploads migration file

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedInteger('file_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('file_id')->references('id')->on('file_uploads')->onDelete('cascade');
         });
     }
 
