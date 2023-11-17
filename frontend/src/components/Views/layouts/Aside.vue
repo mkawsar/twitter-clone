10ecba584c1019ce5e9de26728ff04feb8af808e
mkawsar <kawsar.diu888@gmail.com>
2023-11-17 14:58:31 +0600

    router link added index key

--- frontend/src/components/Views/layouts/Aside.vue
+++ frontend/src/components/Views/layouts/Aside.vue
@@ -5,7 +5,7 @@
                 <i class="fab fa-twitter"></i>
             </button>
             <div>
-                <router-link v-for="(tab,index) in tabs" :to="tab.path" :class="`focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 ${ id === tab.id ? 'text-blue' : ''}`">
+                <router-link v-for="(tab,index) in tabs" :key="index" :to="tab.path" :class="`focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 ${ id === tab.id ? 'text-blue' : ''}`">
                     <i :class="`${ tab.icon } text-2xl mr-4 text-left`"></i>
                     <p class="text-lg font-semibold text-left hidden lg:block"> {{ tab.title }} </p>
                 </router-link>
